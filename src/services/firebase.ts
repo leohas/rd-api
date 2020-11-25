278acb799ad98972b0c09cebef2b29fe7ff3bb95
baphry <jcgf1@ifal.edu.br>
2020-11-25 20:49:52 -0300

    Adds databaseURL from venv

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,7 +2,7 @@ import * as admin from 'firebase-admin'
 
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
-    databaseURL: 'https://rd-alagoana.firebaseio.com'
+    databaseURL: process.env.DATABASE_URL
 })
 
 export const fbRef = admin.database().ref()
