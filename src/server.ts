dbb462286962cdbaddf9ace2fa3687fca90c171f
baphry <jcgf1@ifal.edu.br>
2020-11-25 21:13:50 -0300

    initializes firebase project

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     return res.json({ status: 'ok' })
 })
 
