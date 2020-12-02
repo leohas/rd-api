abe19b2af366095c76433db88698efcfedd1e8f8
Your Name <alexandre.acrsf@gmail.com>
2020-12-02 12:53:12 -0300

    Adicionando rota que mostra a versão da api

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import multer from 'multer'
+import { fbRef } from './services/firebase'
+
 
 const app = express()
 
@@ -15,6 +17,16 @@ app.get('/', (req: Request, res: Response) => {
     return res.json({ status: 'ok' })
 })
 
+app.get('/version', (req: Request, res: Response) => {
+    const version = require('../package.json').version; 
+    
+    res.send(`Versão atual:${version}`);
+    
+})
+app.post('/teste', async (req: Request, res:Response) => {
+    await fbRef.child('/teste').set('teste');
+    return res.send('Funcionou');
+})
 app.listen(1234, () => {
     console.log('server is already running at localhost:' + 1234)
 })
